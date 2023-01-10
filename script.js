c97f8187b506b8d82238c50f10d440a9b3023bbb
Alex Vong <avong003@gmail.com>
2023-01-10 15:30:28 -0800

    all query selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#search-form'),
+    input = document.querySelector('#search-input'),
+    ul = document.querySelector('.results-ul'),
+    landing = document.querySelector('.landing'),
+    searchFeedback = document.querySelector('#search-feedback'),
+    dropDown = document.querySelector('.search-dd'),
+    navSearch = document.querySelector('.nav-search'),
+    closeSearch = document.querySelector('i.close-search'),
+    gridContainer = document.querySelector('.show-grid');
+
